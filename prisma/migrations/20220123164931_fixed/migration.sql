96d493fb7360a81adf4c8499b26e99b02266f14b
Jake <jake.ord345@gmail.com>
2022-01-23 16:49:37 +0000

    fixed schema

---
+++ prisma/migrations/20220123164931_fixed/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Comment` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Like` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Comment";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Like";
+PRAGMA foreign_keys=on;
